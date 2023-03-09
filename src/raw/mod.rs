c5feb8bffcb9027153ef67c55e9a56bfc214cb0a
Shou Ya <shouya@users.noreply.github.com>
2023-03-09 23:28:06 +0800

    add serial component

--- src/raw/mod.rs
+++ src/raw/mod.rs
@@ -1,5 +1,7 @@
 pub mod led;
 pub mod microphone;
+pub mod serial;
 
 pub use led::LedMatrix;
 pub use microphone::Microphone;
+pub use serial::Serial;
---
+++ src/raw/serial.rs
@@ -0,0 +1,36 @@
+#![allow(unused)]
+
+use core::fmt::Write;
+
+use microbit::{
+  board::UartPins,
+  hal::uarte::{self, Instance, Parity, UarteRx, UarteTx},
+};
+
+static mut TX_BUF: [u8; 1] = [0];
+static mut RX_BUF: [u8; 1] = [0];
+
+pub struct Serial<T: Instance> {
+  rx: UarteRx<T>,
+  tx: UarteTx<T>,
+}
+
+impl<T: Instance> Serial<T> {
+  pub fn setup(uarte_reg: T, pins: UartPins) -> Self {
+    let uarte = uarte::Uarte::new(
+      uarte_reg,
+      pins.into(),
+      Parity::EXCLUDED,
+      microbit::hal::uarte::Baudrate::BAUD115200,
+    );
+
+    let (tx, rx) = unsafe { uarte.split(&mut TX_BUF, &mut RX_BUF).unwrap() };
+    Self { tx, rx }
+  }
+
+  pub fn send_str(&mut self, s: &str) {
+    for c in s.chars() {
+      self.tx.write_char(c).unwrap();
+    }
+  }
+}
